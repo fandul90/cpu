ff943e1bc0815bd59c90194bab7c1ce4446ea24b
fandul90 <46699569+fandul90@users.noreply.github.com>
2021-03-14 13:11:10 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
-
+./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.eu.mine.zpool.ca:6242 -u DRNSR4GhrYv8nMXY8uiajQ8KN6bmLunF7gp -t8 -p c=DOGE
 
 done
